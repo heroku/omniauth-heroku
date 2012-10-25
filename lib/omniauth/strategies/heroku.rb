c189592c6a94dd090bfae014c15ecc7c5f6f8a79
Jonathan Dance <jd@wuputah.com>
2012-10-25 01:14:49 -0700

    remove extra info / api call

--- lib/omniauth/strategies/heroku.rb
+++ lib/omniauth/strategies/heroku.rb
@@ -10,25 +10,6 @@ module OmniAuth
         :authorize_url => "#{BaseAuthUrl}/oauth/authorize",
         :token_url => "#{BaseAuthUrl}/oauth/token"
       }
-
-      def request_phase
-        super
-      end
-
-      uid { raw_info['id'] }
-
-      info do
-        { 'email' => raw_info['email'] }
-      end
-
-      extra do
-        { 'raw_info' => raw_info }
-      end
-
-      def raw_info
-        access_token.options[:mode] = :header
-        @raw_info ||= access_token.get('/account').parsed
-      end
     end
   end
 end

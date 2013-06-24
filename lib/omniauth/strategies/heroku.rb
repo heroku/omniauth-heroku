44c16f03d8b9e7af10eb249d93b3060c254bc2c8
Brandon Liu <bdon@bdon.org>
2013-06-24 15:18:34 -0700

    Parse user information from /account into OmniAuth hash.

--- lib/omniauth/strategies/heroku.rb
+++ lib/omniauth/strategies/heroku.rb
@@ -10,6 +10,25 @@ module OmniAuth
         :authorize_url => "#{BaseAuthUrl}/oauth/authorize",
         :token_url => "#{BaseAuthUrl}/oauth/token"
       }
+
+      uid do
+        raw_info['id']
+      end
+
+      info do
+        {
+          'name' => raw_info['name'],
+          'email' => raw_info['email']
+        }
+      end
+
+      extra do
+        { :raw_info => raw_info }
+      end
+
+      def raw_info
+        @raw_info ||= access_token.get('account').parsed
+      end
     end
   end
 end
